834ffc3529126614bbdb930a7a642ec5f077f92c
christopher-lee <clee651@terpmail.umd.edu>
2024-10-03 12:39:35 -0400

    Updated logic: switched to Queue, among other changes

--- fremennik-philharmonic-duo/src/App.jsx
+++ fremennik-philharmonic-duo/src/App.jsx
@@ -7,30 +7,35 @@ import Title from './components/Title'
 import Intro from './components/Intro'
 import Guide from './components/Guide'
 import Music from './components/Music'
+import Button from './components/Button'
 import TrackList from './components/TrackList'
 import SearchBox from './components/SearchBox'
 
 const App = () => {
-  const [track, setTrack] = useState(musicData[0])  // default track to 'Peace and Prosperity'
   const [search, setSearch] = useState('')
-  const [audioObj, setAudioObj] = useState('')
+  const [currentSong, setCurrentSong] = useState(0)
+  const [queue, setQueue] = useState([])
+  const [play, setPlay] = useState(false)
 
-  useEffect(() => {
-    setAudioObj(new Audio(track.url)
-  )}, [track])
-
-  const playTrack = () => {
-    audioObj.play()
+  const handlePlayPause = (event) => {
+    const action = play ? "Pausing" : "Playing"
+    console.log(action)
+    setPlay(!play)
   }
 
-  const pauseTrack = () => {
-    audioObj.pause()
+  const handleNext = () => {
+    // check if next is a valid action
+    if (queue.length > currentSong + 1) {
+      // pause the current song
+      queue[currentSong].pause()
+      // move to the next song
+      setCurrentSong(currentSong + 1)
+    }
   }
 
-  const handleTrackSelection = (event) => {
-    audioObj.pause() // stop the current track
+  const addToQueue = (event) => {
     const newTrack = musicData.find(trackObj => trackObj.title === event.target.id)
-    setTrack(newTrack)
+    setQueue(queue.concat(new Audio(newTrack.url)))
   }
 
   const handleSearch = event => setSearch(event.target.value)
@@ -39,14 +44,36 @@ const App = () => {
     ? musicData
     : musicData.filter(trackObject => trackObject.title.toLowerCase().includes(search.toLowerCase()))
 
+  // Check if the user is allowed to play a song
+  if (queue.length > 0 && currentSong < queue.length) {
+    // Check if the user wants to play or pause
+    play ? queue[currentSong].play() : queue[currentSong].pause()
+  }
+
   return (
     <>
+      <div>Debug: {play ? "Playing" : "Paused"}</div>
       <Title />
       <Intro />
       <Guide />
-      <Music playTrack={playTrack} pauseTrack={pauseTrack} />
-      <SearchBox search={search} handleSearch={handleSearch} />
-      <TrackList searchOptions={searchOptions} handleTrackSelection={handleTrackSelection} />
+      <div>
+        <Button id="play-pause-button" text={play? "Pause" : "Play"} handleClick={handlePlayPause} />
+        <Button id="next-button" text="Next" handleClick={handleNext} />
+      </div>
+      <div>
+        <label htmlFor="search">Search:</label>
+        <input id='search' value={search} onChange={handleSearch}/>
+      </div>
+      <div>
+        {searchOptions.map((trackObject) => {
+          return (
+            <p key={trackObject.id}>
+              {trackObject.title}
+              <Button id={trackObject.title} text={`Queue`} handleClick={addToQueue} />
+            </p>
+          )
+        })}
+      </div>
     </>
   )
 }
